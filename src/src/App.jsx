02ab6ecf4b31b3d2b61cfa3cb426d0a3dcf28f0a
Dhirendra Maurya <dhirendram128@gmail.com>
2025-10-19 12:28:05 +0530

    Create initial App component with welcome message

---
+++ src/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 ProfileClaim Starter</h1>
+      <p>Your app is running successfully on Vercel!</p>
+    </div>
+  );
+}
